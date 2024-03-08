8bf4dc5f840fa6b83ceba8a85a647fdc610a6260
Trần Minh Quang <tmquang0209@gmail.com>
2024-03-08 17:12:59 +0700

    Update package.json and controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -177,7 +177,7 @@ const scheduleController = async () => {
 
     //Get enrollId and enrollPassword from localStorage
     const enrollId = localStorage.get("enrollId");
-    if (!enrollId) localStorage.set("enrollId", parseEnrollId, 60);
+    if (!enrollId || enrollId != parseEnrollId) localStorage.set("enrollId", parseEnrollId, 60);
     const enrollPassword = localStorage.get("enrollPassword");
 
     //If id in the URL is not equal to id in localStorage => Skip typing the password
